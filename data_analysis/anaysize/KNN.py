f5f7364f8297e72bc6807849b408b2537f7dd928
Chandler <chandler@ChandlerdeMacBook-Pro.local>
2017-09-23 21:24:53 +0800

    machine learning

--- data_analysis/anaysize/KNN.py
+++ data_analysis/anaysize/KNN.py
@@ -5,16 +5,34 @@ from os import listdir
 
 
 def img2vector(filename):
-    returnVect = zeros((1,1024))
+    returnvect = zeros((1, 1024))
     fr = open(filename)
     for i in range(32):
-        lineStr = fr.readline()
+        line_str = fr.readline()
         for j in range(32):
-            returnVect[0,32*i+j] = int(lineStr[j])
-    return returnVect
+            returnvect[0, 32*i+j] = int(line_str[j])
+    return returnvect
 
 file = "/Users/yinchuchu/Desktop/MachineLearning-master/kNN/use_Python_and_NumPy/trainingDigits/6_1.txt"
 
+
+# 分类主体程序，计算欧式距离，选择距离最小的k个，返回k个中出现频率最高的类别
+# inX是所要测试的向量
+# dataSet是训练样本集，一行对应一个样本。dataSet对应的标签向量为labels
+# k是所选的最近邻数目
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]                       # shape[0]得出dataSet的行数，即样本个数
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet       # tile(A,(m,n))将数组A作为元素构造m行n列的数组
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)                  # array.sum(axis=1)按行累加，axis=0为按列累加
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()             # array.argsort()，得到每个元素的排序序号
+    classCount={}                                        # sortedDistIndicies[0]表示排序后排在第一个的那个数在原来数组中的下标
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1  # get(key,x)从字典中获取key对应的value，没有key的话返回0
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)  # sorted()函数，按照第二个元素即value的次序逆向（reverse=True）排序
+    return sortedClassCount[0][0]
 
 
 def handwritingClassTest():
@@ -46,21 +64,3 @@ def handwritingClassTest():
     print("\nthe total error rate is: %f" % (errorCount / float(mTest)))
 
 
-
-#分类主体程序，计算欧式距离，选择距离最小的k个，返回k个中出现频率最高的类别
-#inX是所要测试的向量
-#dataSet是训练样本集，一行对应一个样本。dataSet对应的标签向量为labels
-#k是所选的最近邻数目
-def classify0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]                       #shape[0]得出dataSet的行数，即样本个数
-    diffMat = tile(inX, (dataSetSize,1)) - dataSet       #tile(A,(m,n))将数组A作为元素构造m行n列的数组
-    sqDiffMat = diffMat**2
-    sqDistances = sqDiffMat.sum(axis=1)                  #array.sum(axis=1)按行累加，axis=0为按列累加
-    distances = sqDistances**0.5
-    sortedDistIndicies = distances.argsort()             #array.argsort()，得到每个元素的排序序号
-    classCount={}                                        #sortedDistIndicies[0]表示排序后排在第一个的那个数在原来数组中的下标
-    for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]
-        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1 #get(key,x)从字典中获取key对应的value，没有key的话返回0
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True) #sorted()函数，按照第二个元素即value的次序逆向（reverse=True）排序
-    return sortedClassCount[0][0]
