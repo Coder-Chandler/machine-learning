f1180db2d9ae558bf6c1c1b46d83f5a866139e83
Chandler <ysiwgtus@gmail.com>
2017-09-25 22:40:00 +0800

    machine learning
    machine learning

--- data_analysis/anaysize/KNN.py
+++ data_analysis/anaysize/KNN.py
@@ -19,8 +19,8 @@ def featurevector(file):
     return feature_vector
 
 
-file = "/Users/chandler/Documents/Projects/machine-learning/data_analysis/anaysize/trainingDigits/3_9.txt"
-print(featurevector(file))
+# file = "/Users/chandler/Documents/Projects/machine-learning/data_analysis/anaysize/trainingfile/3_9.txt"
+# print(featurevector(file))
 
 
 # 转换成特征向量之后测试转换后的图片
@@ -29,79 +29,107 @@ def test_featurevector(feature_vector):
         for count in range(32):
             test = list(value[32*count:32*count+32])
             print(test)
+# file = "/Users/chandler/Documents/Projects/machine-learning/data_analysis/anaysize/trainingfile/3_9.txt"
 # test_featurevector(featurevector(file))
 
 
-# 分类主体程序，计算欧式距离，选择距离最小的k个，返回k个中出现频率最高的类别
-# inX是所要测试的向量
-# dataSet是训练样本集，一行对应一个样本。dataSet对应的标签向量为labels
-# k是所选的最近邻数目
-# classify0(vectorUnderTest, trainingMat, hwLabels, 3)
-def classify0(inX, dataSet, labels, k):
-    print("所要测试的向量inX : %s" % inX)
-    print("训练样本集dataSet : %s" % dataSet)
-    print("训练样本集对应的标签向量labels : %s" % labels)
+# 欧氏距离公式
+def euclidean_distance(x, y):
+    # 首先求平方
+    sq = (x - y)**2
+    # 再对平方结果求和并且开方
+    distance = (sq.sum(axis=1))**0.5
+    return distance
+
+
+def classify(testfile_matrix, trainingfile_matrix, handwriting_labels, k):
+    print("所要测试的向量testfile_matrix : %s" % testfile_matrix)
+    print("训练样本集trainingfile_matrix : %s" % trainingfile_matrix)
+    print("训练样本集对应的文件标签（0, 1, 2, 3.....9）handwriting_Labels : %s" % handwriting_labels)
     print("最近邻居数目 : %s" % k)
-    dataSetSize = dataSet.shape[0]  # shape[0]得出dataSet的行数，即样本个数
-    print("1、训练样本集的行数（即样本个数）: %s" % dataSetSize)
-    diffMat = tile(inX, (dataSetSize, 1)) - dataSet  # tile(A,(m,n))将数组A作为元素构造m行n列的数组
-    print("2、将所要测试的向量作为元素构造以样本个数为行，1列的数组 : %s" % diffMat)
-    sqDiffMat = diffMat**2
-    print("3、对第2步求2次幂 : %s" % sqDiffMat)
-    sqDistances = sqDiffMat.sum(axis=1)  # array.sum(axis=1)按行累加，axis=0为按列累加
-    print("4、对第3步按行累加 : %s" % sqDistances)
-    distances = sqDistances**0.5
-    print("5、对第4步开根号得到距离集合 : %s" % distances)
-    sortedDistIndicies = distances.argsort()  # array.argsort()，得到每个元素的排序序号
-    print("得到每个元素的排序序号（距离按照从小到大排序） : %s" % sortedDistIndicies)
-    classCount={}  # sortedDistIndicies[0]表示排序后排在第一个的那个数在原来数组中的下标
+    trainingfile_matrix_size = trainingfile_matrix.shape[0]
+    print("1、训练样本集的行数（即样本个数）: %s" % trainingfile_matrix_size)
+    testfile_array = tile(testfile_matrix, (trainingfile_matrix_size, 1))
+    print("2、将所要测试的向量作为元素构造以样本个数为行，1列的数组testfile_array : %s" % testfile_array)
+    testfile_trainingfile_diff = testfile_array - trainingfile_matrix
+    print("3、将数组testfile_array减去训练样本集矩阵数组trainingfile_matrix : %s" % testfile_trainingfile_diff)
+    trainingfile_matrix_array_square = testfile_trainingfile_diff**2
+    print("4、对第2步求2次幂 : %s" % trainingfile_matrix_array_square)
+    # array.sum(axis=1)按行累加，axis=0为按列累加
+    trainingfile_matrix_array_sum = trainingfile_matrix_array_square.sum(axis=1)
+    print("5、对第3步的每一行单独求和 : %s" % trainingfile_matrix_array_sum)
+    euclidean_distance = trainingfile_matrix_array_sum**0.5
+    print("6、对第4步开根号得到距离集合 : %s" % euclidean_distance)
+    # array.argsort()，得到每个元素的排序序号
+    sort_euclidean_distance = euclidean_distance.argsort()
+    print("得到每个元素的排序序号集合（距离按照从小到大排序） : %s" % sort_euclidean_distance)
+    knn_dict={}
     for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]
-        print("voteIlabel : %s" % voteIlabel)
-        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1  # get(key,x)从字典中获取key对应的value，没有key的话返回0
-        print("classCount : %s" % classCount)
-    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)  # sorted()函数，按照第二个元素即value的次序逆向（reverse=True）排序
-    print("sortedClassCount : %s" % sortedClassCount)
-    return sortedClassCount[0][0]
+        """
+        k若等于3，那么就拿出sort_euclidean_distance的前三个下标元素（距离最小的前三个元素）
+        并且按照这前三个下标找到在handwriting_Labels中对应下标的手写数字没，这个数字就是NearestNeighbor，最近邻居，当然一共有3个
+        """
+        nearest_neighbor = handwriting_labels[sort_euclidean_distance[i]]
+        print("NearestNeighbor : %s" % nearest_neighbor)
+        """
+        KNN_dict就是用来计数的，三个邻居谁出现频率高就确定测试数字是和哪个邻居一个类别，即同一个数字
+        """
+        knn_dict[nearest_neighbor] = knn_dict.get(nearest_neighbor, 0) + 1
+        print("KNN_dict : %s" % knn_dict)
+    # sorted()函数，按照第二个元素即value的次序逆向（reverse=True）排序
+    sorted_knn_dict = sorted(knn_dict.items(), key=operator.itemgetter(1), reverse=True)
+    print("最近邻居出现频率sorted_KNN_dict : %s" % sorted_knn_dict)
+    return sorted_knn_dict[0][0]
 
 
-def HandWriting():
-    # 加载训练集到大矩阵trainingMat
-    hwLabels = []
-    training_file_list = listdir('trainingDigits')  # os模块中的listdir('str')可以读取目录str下的所有文件名，返回一个字符串列表
+def handwriting_guess():
+    # 加载训练集到矩阵中
+    handwriting_labels = []
+    # os模块中的listdir('str')可以读取目录str下的所有文件名，返回一个字符串列表
+    training_file_list = listdir('trainingfile')
     print("所有训练集文件 : %s" % training_file_list)
-    row = len(training_file_list)  # 矩阵的行数就是trainingDigits文件的个数
+    # 矩阵的行数就是trainingfile文件的个数
+    row = len(training_file_list)
     print("所有训练集文件的个数 : %s" % row)
-    trainingdigits_matrix = zeros((row, 1024))
-    print("生成训练集大矩阵 : %s" % trainingdigits_matrix)
+    trainingfile_matrix = zeros((row, 1024))
+    print("生成训练集矩阵 : %s" % trainingfile_matrix)
+
     for i in range(row):
-        training_file_name = training_file_list[i]  # 遍历训练集中所有训练文件：1_120.txt
-        training_file = training_file_name.split('.')[0]  # 去除.txt后缀, 1_120.txt --> 1_120
-        training_digit = int(training_file.split('_')[0])  # 以_切片，得到1，1_120 --> 1
-        hwLabels.append(training_digit)
-        trainingdigits_matrix[i, :] = featurevector('trainingDigits/%s' % training_file_name)  # 将遍历的每一个训练文件转换为特征向量
-    print("处理训练集文件名labels : %s" % hwLabels)
-    print("将所有训练文件转换为特征向量dataSet : %s" % trainingdigits_matrix)
+        # 遍历训练集中所有训练文件：1_120.txt
+        training_file_name = training_file_list[i]
+        # 去除.txt后缀, 1_120.txt --> 1_120
+        training_file = training_file_name.split('.')[0]
+        # 以_切片，得到1，1_120 --> 1
+        training_digit = int(training_file.split('_')[0])
+        # 把处理好的文件名放进列表handwriting_labels中待后用
+        handwriting_labels.append(training_digit)
+        # 将遍历的每一个训练文件转换为特征向量
+        trainingfile_matrix[i, :] = featurevector('trainingfile/%s' % training_file_name)
+    print("处理训练集文件名handwriting_Labels : %s" % handwriting_labels)
+    print("将所有训练文件转换为特征向量trainingfile_matrix : %s" % trainingfile_matrix)
 
     # 逐一读取测试图片，同时将其分类
-    testfile_list = listdir('testDigits')
+    testfile_list = listdir('testfile')
     print("测试集 : %s" % testfile_list)
     error_count = 0.0
     testfile_nums = len(testfile_list)
     print(" 测试集个数 : %s" % testfile_nums)
+
     for i in range(testfile_nums):
         testfile_name = testfile_list[i]
         testfile = testfile_name.split('.')[0]
         test_digit = int(testfile.split('_')[0])
-        vectorUnderTest = featurevector('testDigits/%s' % testfile_name)
-        print("将每一个测试集文件准换成特征向量inX: %s" % vectorUnderTest)
-        classifierResult = classify0(vectorUnderTest, trainingdigits_matrix, hwLabels, 3)
-        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, test_digit))
-        if (classifierResult != test_digit): error_count += 1.0
-    print("\nthe total number of errors is: %d" % error_count)
-    print("\nthe total error rate is: %f" % (error_count / float(testfile_nums)))
-
-
+        # 将遍历的每一个测试文件转换为特征向量
+        testfile_matrix = featurevector('testfile/%s' % testfile_name)
+        print("将每一个测试集文件准换成特征向量testfile_matrix: %s" % testfile_matrix)
+        knn_result = classify(testfile_matrix, trainingfile_matrix, handwriting_labels, 3)
+        print("KNN识别的结果: %d, 真实的结果: %d" % (knn_result, test_digit))
+        if knn_result != test_digit:
+            error_count += 1.0
+    print("\nthe 识别出错数量: %d" % error_count)
+    print("\nthe 识别错误率: %f" % (error_count / float(testfile_nums)))
+# 运行KNN算法识别手写数字
+handwriting_guess()
 
 
 
